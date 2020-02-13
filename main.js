12de12d8795f2fee0a6b5bdc0b97e8bb4015c31f
lazzlo2096 <lazzlo2096@yandex.ru>
2020-02-13 21:16:00 +0300

    add keyboard control;

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
 const canvas = document.getElementById("pong-canvas");
 const context = canvas.getContext("2d");
 
-//var MAX_SCREEN = 600; // canvas.getAttribute('width')
+document.addEventListener("keydown" , eventsProcess );
 
-var pos1 = {x: 10, y: 0, angle: 0}; // 'y' нужен только если используем и angle
+var distanceFromEdge = 20;
 
-var border1 = newBorder(pos1, canvas.getAttribute('width'));
+var posOfPlayersBorder = {x: canvas.getAttribute('width') - distanceFromEdge - 20  , y: 0, angle: 0};
+var playersBorder = newBorder(posOfPlayersBorder, canvas.getAttribute('width'));
 
-//while (true)
-{
-    border1.draw();
-    border1.moveRigth();
-    border1.moveRigth();
-    border1.draw();
+var posOfEnemysBorder = {x: distanceFromEdge, y: 0, angle: 0};
+var EnemysBorder = newBorder(posOfEnemysBorder, canvas.getAttribute('width'));
+
+const fps = 60;
+setInterval(renderGame, 1000/fps);
+
+function eventsProcess(event) {
+    if (event.isComposing || event.keyCode === 229) {
+        return;
+    }
+
+    switch(event.code){
+        case 'ArrowDown' : playersBorder.moveRigth(); break;
+        case 'ArrowUp' : playersBorder.moveLeft(); break;
+    }
+}
+
+function renderGame() {
+    clearCanvas();
+
+    playersBorder.draw();
+    EnemysBorder.draw();
+}
+
+function clearCanvas() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, canvas.getAttribute('width'), canvas.getAttribute('height'));
 }
 
 function newBorder(position, span) {
 
     var border = {};
     
-    border.color = "black";
+    border.color = "white";
     border.width = 20;
     border.lenght = 120;
     
@@ -37,11 +59,22 @@ function newBorder(position, span) {
         );
     }
 
-    border.moveRigth = function(){
-        var increment = 2;
-        
-        if (this.positionAtSpan+increment <= this.span) {
+    var increment = 10;
+
+    border.moveRigth = function() {
+        //var increment = 10;
+        if (this.positionAtSpan+(this.lenght/2)+increment <= this.span) {
             this.positionAtSpan += increment;
+        }else{
+            this.positionAtSpan = this.span-(this.lenght/2)
+        }
+    }
+    border.moveLeft = function() {
+        //var increment = 10;
+        if (this.positionAtSpan-(this.lenght/2)-increment >= 0) {
+            this.positionAtSpan -= increment;
+        }else{
+            this.positionAtSpan = 0+(this.lenght/2);
         }
     }
 
